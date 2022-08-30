6af73b5f74df0dbec028abbd1eacca4b173c1b6d
2cheinz <cheinzwebdev@gmail.com>
2022-08-29 20:48:51 -0500

    emojiart.py

---
+++ emojiart.py
@@ -0,0 +1,13 @@
+# Emoji Art exercise practice with for and while loops
+
+for i in range(3):
+    for num in range(1,11):
+        print("\U0001f600" * num)
+
+# While loop
+
+art = 1
+
+while art < 11:
+    print("\U0001f600" * art)
+    art += 1
